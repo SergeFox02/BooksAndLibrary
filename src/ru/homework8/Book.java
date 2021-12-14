bc3be0ac0ee5a01a4f6bc370d51cce43af976baa
SergeFox02 <Fox2188@gmail.com>
2021-12-14 23:26:29 +0300

    Третий коммит. Решил задание среднего уровня. Вернее усовершенствовал. Так же добавил в конструктор Book поле name.

--- src/ru/homework8/Book.java
+++ src/ru/homework8/Book.java
@@ -3,15 +3,25 @@ package ru.homework8;
 public class Book {
 
     private final Author author;
-    int publisherYear;
+    private final String name;
+    private int publisherYear;
 
-    public Book(Author author, int publisherYear) {
+    public Book(Author author, String name, int publisherYear) {
         this.author = author;
+        this.name = name;
         this.publisherYear = publisherYear;
+    }
+
+    public Book(Author author, String name) {
+        this(author, name, 0);
     }
 
     public Author getAuthor() {
         return this.author;
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     public int getPublisherYear() {
@@ -23,6 +33,6 @@ public class Book {
     }
 
     public void print() {
-        System.out.println(author.getFirstName() + " " + author.getLastName() + " " + this.publisherYear);
+        System.out.println(author.getFirstName() + " " + author.getLastName() + ": " + this.name + ": " + this.publisherYear);
     }
 }
--- src/ru/homework8/Main.java
+++ src/ru/homework8/Main.java
@@ -7,19 +7,36 @@ public class Main {
         Author tolstoi = new Author("Lev", "Tolstoi");
         Author dostoevskii = new Author("Fedor", "Dostoevsky");
 
-        tolstoi.print();
-        dostoevskii.print();
+        Book[] books = new Book[5];
 
-        Book warAndPeace = new Book(tolstoi, 1865);
-        Book idiot = new Book(dostoevskii, 1868);
+        Book warAndPeace = new Book(tolstoi, "War and Peace");
+        Book idiot = new Book(dostoevskii, "Idiot", 1868);
 
         System.out.println("warAndPeace.getPublisherYear() = " + warAndPeace.getPublisherYear());
-
         warAndPeace.setPublisherYear(1869);
 
-        warAndPeace.print();
-        idiot.print();
-
+        setBook(books, warAndPeace);
+        setBook(books, warAndPeace);
+        setBook(books, idiot);
+        printBooks(books);
 
     }
+
+    public static void setBook(Book[] books, Book book) {
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] == null) {
+                books[i] = book;
+                return;
+            }
+        }
+        System.out.println("В библиотеке нет места!");
+    }
+
+    public static void printBooks(Book[] books) {
+        for (int i = 0; i < books.length; i++)
+            if (books[i] != null) {
+                books[i].print();
+            }
+    }
+
 }
